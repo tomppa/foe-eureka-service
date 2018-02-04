e242e9dfc97fdc1e5351c8b54e005b59857e3fb1
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2018-02-04 16:48:45 +0800

    Adding files generated by Spring Initializr.

---
+++ src/main/java/com/github/tomppa/foe/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.tomppa.foe.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+}
